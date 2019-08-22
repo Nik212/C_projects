ad5fdd575ba4ccafc0e650a0eed15898df41986e
Shaunta Lupe <slupe@ga-k4.21-school.ru>
2019-08-22 14:16:45 +0300

    WTF

--- linked lists/display.c
+++ linked lists/display.c
@@ -47,11 +47,40 @@ void recursive_display(struct Node *p)
         printf("No list");
 
 }
+int asccending(int a, int b)
+{
+    return (a<=b);
+}
+void ft_swap(int *a, int *b)
+{
+    int tmp;
+    tmp = *b;
+    *b = *a;
+    *a = tmp;
+}
+struct Node *sort(struct Node *lst)
+{
+    struct Node *last = lst;
+    struct Node *start;
+
+    start = lst;
+    lst = lst->next;
+    while (lst)
+    {
+        if (!(asccending(last->data, lst->data)))
+            ft_swap(&lst->data, &last->data);
+        last = lst;
+        lst = lst->next;
+        // [] ---> [5, 0] --> [3, 1] --> [7, 2] // lst = [] '' if lst->data = 1 >= 1->data = 2 then lst->data
+    }
+    return (start);
+}
 
 int main()
 {
-    int A[] = {3, 5, 7, 10, 15};
+    int A[] = {5, 7, 10, 3, 4};
 
     create(A, 5);
+    sort(first);
     return (0);
 }
